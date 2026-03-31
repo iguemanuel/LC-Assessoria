ca93ca8bad7f7c4b817fba63642ceaa78fb4c6e9
Igor <igor17emanuel@gmail.com>
2026-03-31 19:37:39 -0300

    feat: adiciona botao flutuante do WhatsApp

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import AppHeader from '@/components/AppHeader.vue'
 import AppFooter from '@/components/AppFooter.vue'
+import WhatsAppFloat from '@/components/WhatsAppFloat.vue'
 </script>
 
 <template>
   <AppHeader />
   <RouterView />
   <AppFooter />
+  <WhatsAppFloat />
 </template>
 
 <style scoped></style>
